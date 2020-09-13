fdf514ba83c4c84e4778256f36cebeabf70c40c6
ppadron5 <mw3issocool@gmail.com>
2020-09-13 18:30:15 -0500

    Got arg passing functioning

--- Assignment1/wordcount.c
+++ Assignment1/wordcount.c
@@ -1,32 +1,37 @@
 #include <stdio.h>
 
-void wordcount(char *argFile);
+void wordcount(char *txtFile);
 
 int main(int argc, char *argv[])
 {
     int i;
 
-    printf("Number of args is %d\n", argc);
+    printf("Number of file args is %d\n", argc - 1);
+
+    if( argc < 2)
+    {
+        printf("Too few arguments, please pass a file\n");
+    }
 
     for(i = 1; i < argc; i++)
     {
-        //printf("%s\n", argv[i]);
+        printf("%s\n", argv[i]);
         wordcount(argv[i]);
     }
 
     return 0;
 }
 
-void wordcount(argFile)
+void wordcount(char *txtFile)
 {
     int iCount = 0;
     char cLetter;
     FILE *pFile;
 
-    printf("%s\n", argFile);
-    /*pFile = fopen(arg, "r");
+    printf("%s\n", txtFile);
+    pFile = fopen(txtFile, "r");
 
-    while((cLetter = fgets(pFile)) != EOF)
+    /*while((cLetter = fgets(pFile)) != EOF)
     {
         if(cLetter == ' ' || cLetter == '\n')
         {
@@ -35,6 +40,5 @@ void wordcount(argFile)
     }
 
     printf("Word count is %d", iCount);
-    fclose(pFile);
-    */
+    fclose(pFile);*/
 }
