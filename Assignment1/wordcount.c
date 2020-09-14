9bf0ef722fcced749a47b9a15f1dbda70e25378d
ppadron5 <mw3issocool@gmail.com>
2020-09-13 19:35:07 -0500

    Added comments

--- Assignment1/wordcount.c
+++ Assignment1/wordcount.c
@@ -1,5 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h> 
+#include <unistd.h> 
 
+/*
+This program is to showcase and demonstrate fork(), make system calls, and perform file operations.
+The program has two functions, the main function and the wordcount function. The main function handles
+the check for the amount of arguments as well as iterating through the list of arguments to fork and
+call the wordcount function.
+The wordcount function counts the number of words in supplied file by using fopen to open the file passed
+to it. Once the file is open it then checks each character to see if it is a space, newline, null terminator,
+or tab. If it is any one of these it increases the word count. Once finished counting it prints the file name
+as well as the wordcount the function found.
+*/
+
+/*Prototype for the worcount function.
+It is a void function since it only prints out the filename and number of words.
+It has one argument that is the file to be counted.
+*/
 void wordcount(char *txtFile);
 
 int main(int argc, char *argv[])
@@ -15,9 +34,10 @@ int main(int argc, char *argv[])
 
     for(i = 1; i < argc; i++)
     {
-        printf("%s\n", argv[i]);
+        //printf("%s\n", argv[i]);
+        fork();
         wordcount(argv[i]);
-    }
+    }while(wait(NULL) != -1);
 
     return 0;
 }
@@ -31,14 +51,14 @@ void wordcount(char *txtFile)
     printf("%s\n", txtFile);
     pFile = fopen(txtFile, "r");
 
-    /*while((cLetter = fgets(pFile)) != EOF)
+    while((cLetter = fgetc(pFile)) != EOF)
     {
-        if(cLetter == ' ' || cLetter == '\n')
+        if(cLetter == ' ' || cLetter == '\n' || cLetter == '\0')
         {
             iCount++;
         }
     }
 
-    printf("Word count is %d", iCount);
-    fclose(pFile);*/
+    printf("Word count is %d\n", iCount);
+    fclose(pFile);
 }
